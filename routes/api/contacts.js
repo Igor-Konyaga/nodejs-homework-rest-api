ba9c99dc05806d5802494c4a51f9cc7fecb854ce
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-12-17 23:36:46 +0200

    update

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,49 +1,42 @@
 const express = require("express");
-const {
-  listContacts,
-  getContactById,
-  addContact,
-  removeContact,
-  updateContact,
-} = require("../../models/contacts");
 const { createUserValidator } = require("../../utils/userValidators");
 const { httpError } = require("../../helpers/httpError");
+const Contact = require("../../models/contact.model");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  try {
-    const contacts = await listContacts();
+// router.get("/", async (req, res, next) => {
+//   try {
 
-    res.status(200).json(contacts);
-  } catch (error) {
-    next(error);
-  }
-});
+//     res.status(200).json(contacts);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
-router.get("/:contactId", async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
+// router.get("/:contactId", async (req, res, next) => {
+//   try {
+//     const { contactId } = req.params;
+//     const contact = await getContactById(contactId);
 
-    if (!contact) {
-      res.status(404).json({ message: "Not Foun" });
-      return;
-    }
+//     if (!contact) {
+//       res.status(404).json({ message: "Not Foun" });
+//       return;
+//     }
 
-    res.status(200).json(contact);
-  } catch (error) {
-    next(error);
-  }
-});
+//     res.status(200).json(contact);
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 router.post("/", async (req, res, next) => {
   try {
-    const { value, error } = createUserValidator(req.body);
+    //  const { value, error } = createUserValidator(req.body);
 
-    if (error) throw httpError(400, "Invalid user data!");
+    //  if (error) throw httpError(400, "Invalid user data!");
 
-    const newContact = await addContact(value);
+    const newContact = await Contact.create(req.body);
 
     res.status(201).json(newContact);
   } catch (error) {
@@ -51,39 +44,39 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-router.delete("/:contactId", async (req, res, next) => {
-  const { contactId } = req.params;
-  const deleteContact = await removeContact(contactId);
+// router.delete("/:contactId", async (req, res, next) => {
+//   const { contactId } = req.params;
+//   const deleteContact = await removeContact(contactId);
 
-  if (!deleteContact) {
-    res.status(404).json({ message: "Not Found" });
+//   if (!deleteContact) {
+//     res.status(404).json({ message: "Not Found" });
 
-    return;
-  }
+//     return;
+//   }
 
-  res.status(200).json({ message: "contact deleted" });
-});
+//   res.status(200).json({ message: "contact deleted" });
+// });
 
-router.put("/:contactId", async (req, res, next) => {
-  try {
-    const { id } = req.params;
+// router.put("/:contactId", async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
 
-    const { value, error } = createUserValidator(req.body);
+//     const { value, error } = createUserValidator(req.body);
 
-    if (error) throw httpError(400, "Invalid user data!");
+//     if (error) throw httpError(400, "Invalid user data!");
 
-    const contact = await updateContact(id, value);
+//     const contact = await updateContact(id, value);
 
-    if (!contact) {
-      res.status(404).json({ message: "Not Found" });
+//     if (!contact) {
+//       res.status(404).json({ message: "Not Found" });
 
-      return;
-    }
+//       return;
+//     }
 
-    res.status(200).json(contact);
-  } catch (error) {
-    res.status(error.status).json({ message: error.message });
-  }
-});
+//     res.status(200).json(contact);
+//   } catch (error) {
+//     res.status(error.status).json({ message: error.message });
+//   }
+// });
 
 module.exports = router;
